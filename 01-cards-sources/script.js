160e6253b6eae3a76811475aeaa8184725809cd4
D1mon1k <ostrovskiy.a.97@gmail.com>
2021-06-18 15:13:54 +0300

    Учусь делать функционал как на trello

--- 01-cards-sources/script.js
+++ 01-cards-sources/script.js
@@ -1,16 +1,22 @@
 'use strict';
 const slides = document.querySelectorAll('.slide')
 
-slides.forEach((slide, index) => {
-  slide.addEventListener('click', function () {
-    clearActiveClasses()
+function selectSlide(activeSlide = 0) {
+  slides[activeSlide].classList.add('active')
 
-    slide.classList.add('active')
+  slides.forEach((slide, index) => {
+    slide.addEventListener('click', function () {
+      clearActiveClasses()
+
+      slide.classList.add('active')
+    })
   })
-})
 
-function clearActiveClasses() {
-  for (let slide of slides) {
-    slide.classList.remove('active')
+  function clearActiveClasses() {
+    for (let slide of slides) {
+      slide.classList.remove('active')
+    }
   }
 }
+
+selectSlide()
